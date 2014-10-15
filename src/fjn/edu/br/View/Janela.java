03632c07167b7d5397b8afd4f6258b85433671e2
UelioNobre <uelionobre@hotmail.com>
2014-10-15 14:38:54 -0300

    Criacao do metodo que popula o JcomboBox das lojas

--- src/fjn/edu/br/View/Janela.java
+++ src/fjn/edu/br/View/Janela.java
@@ -115,8 +115,12 @@ public class Janela extends JFrame implements ActionListener {
      * Preenche o JTable com os dados do banco de dados
      */
     private void preencheJTable() {
+
         SolicitacaoCompraDAO solicitacaoCompraDAO = new SolicitacaoCompraDAO();
         List<SolicitacaoCompra> solicitacoesCompras = solicitacaoCompraDAO.getSolicitacaoDeCompra(10);
+        
+        // Zera os indices que são usados no defaultaTableModel.
+        intNumRegistro = 0;
 
         for (int i = 0; i < solicitacoesCompras.size(); i++) {
             intRegistro = solicitacoesCompras.get(i).getLojaId();
@@ -137,6 +141,14 @@ public class Janela extends JFrame implements ActionListener {
             this.defaultTableModel.insertRow(intNumRegistro, campo);
             intNumRegistro++;
         }
+
+    }
+
+    // Remove os itens do defaultTableModel
+    public void limpaJTable() {
+        while (this.defaultTableModel.getRowCount() > 0) {
+            this.defaultTableModel.removeRow(0);
+        }
     }
 
     @Override
@@ -154,6 +166,7 @@ public class Janela extends JFrame implements ActionListener {
             // Busca novamente os dados do banco de dados
             // atualiza o jtable
             gerar();
+            this.limpaJTable();
             this.table.repaint();
             this.preencheJTable();
 
@@ -162,7 +175,6 @@ public class Janela extends JFrame implements ActionListener {
         }
         if (e.getSource() == botaoGerarRetorno) {
             System.out.println("Arquivo retorno sendo gerado aguarde");
-            
 
         }
 
