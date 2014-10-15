87b7c289755cb1974165e8a4086397f64d1518cd
UelioNobre <uelionobre@hotmail.com>
2014-10-15 17:03:37 -0300

    Antes de cadastrar as solicitações de compras, verifica se ela já existe no banco de dados. Se já existir não o persiste mais.

--- src/fjn/edu/br/dao/SolicitacaoCompraDAO.java
+++ src/fjn/edu/br/dao/SolicitacaoCompraDAO.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fjn.edu.br.dao;
 
 import fjn.edu.br.Connection.Conn;
@@ -36,6 +31,7 @@ public class SolicitacaoCompraDAO {
 
         /**
          * Antes de cadastrar, verificamos se a compra já existe. Neste caso
+         * se der verdadeiro, então retorne e não deixe cair no try
          */
         if (solicitacaoExiste(compra)) {
             return;
