7c1d6feae52af7bc2130a91652a79d43362cbc26
fillipquesado <esquesado@gmail.com>
2014-10-15 16:19:26 -0300

    Alterando ArquivoRetorno agora gerando todos as parcelas por loja e persistindo no banco as parcelas

--- src/credicard/ArquivoRetorno.java
+++ src/credicard/ArquivoRetorno.java
@@ -7,6 +7,7 @@ package credicard;
 
 import fjn.edu.br.Model.Retorno;
 import fjn.edu.br.Model.SolicitacaoCompra;
+import fjn.edu.br.dao.RetornoDAO;
 import java.io.FileNotFoundException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -33,7 +34,6 @@ public class ArquivoRetorno {
         try {
             Formatter saida = new Formatter(nomeArquivo);
             saida.format(string);
-            saida.format("\r\n");
             saida.close();
         } catch (FileNotFoundException e) {
             System.out.println("Erro ao gerar aquivo " + e.getMessage());
@@ -41,43 +41,46 @@ public class ArquivoRetorno {
     }
 
     public static void gerarArquivoRetorno(List<SolicitacaoCompra> solicitacaoCompra) {
-
+        String aux = "";
         for (SolicitacaoCompra solicitacao : solicitacaoCompra) {
-        Retorno ret = new Retorno();
+            Retorno ret = new Retorno();
 
-        // Calcula o valor da parcela
-        double valorParcela = solicitacao.getValorTotal() / solicitacao.getQtdParcelas();
+            // Calcula o valor da parcela
+            double valorParcela = solicitacao.getValorTotal() / solicitacao.getQtdParcelas();
 
-        ret.setCodigoVenda(solicitacao.getCodigoVenda());
-        ret.setIdCredor(1); // Nosso ID
-        ret.setIdCartao(solicitacao.getCartaoId());
-        ret.setDataEnvio(solicitacao.getDataCompra());
-        ret.setValorParcela(valorParcela);
-        ret.setTotalParcela(solicitacao.getQtdParcelas());
+            ret.setCodigoVenda(solicitacao.getCodigoVenda());
+            ret.setIdCredor(1); // Nosso ID
+            ret.setIdCartao(solicitacao.getCartaoId());
+            ret.setDataEnvio(solicitacao.getDataCompra());
+            ret.setValorParcela(valorParcela);
+            ret.setTotalParcela(solicitacao.getQtdParcelas());
 
-        String[] dataVenda = solicitacao.getDataCompra().split("/");
+            String[] dataVenda = solicitacao.getDataCompra().split("/");
 
-        int dataVendaDia = Integer.parseInt(dataVenda[0]);
-        int dataVendaMes = Integer.parseInt(dataVenda[1]);
-        int dataVendaAno = Integer.parseInt(dataVenda[2]);
-
+            int dataVendaDia = Integer.parseInt(dataVenda[0]);
+            int dataVendaMes = Integer.parseInt(dataVenda[1]);
+            int dataVendaAno = Integer.parseInt(dataVenda[2]);
+            
+            RetornoDAO dao = new RetornoDAO();
         // Deixar para o final... na hora de gravar o arquivo de texto 
-        // ret.setNumeroParcela(numeroParcela);
-        for (int i = 1; i <= solicitacao.getQtdParcelas(); i++) {
-            SimpleDateFormat sd = new SimpleDateFormat("dd/MM/yyyy");
-            Calendar c = new GregorianCalendar(dataVendaAno, dataVendaMes, dataVendaDia);
+            // ret.setNumeroParcela(numeroParcela);
+            for (int i = 1; i <= solicitacao.getQtdParcelas(); i++) {
+                SimpleDateFormat sd = new SimpleDateFormat("dd/MM/yyyy");
+                Calendar c = new GregorianCalendar(dataVendaAno, dataVendaMes, dataVendaDia);
 
-            // Adiciona 30 dias para cada data 
-            c.add(Calendar.DAY_OF_MONTH, 30 * i);
-            String novaDataEnvio = sd.format(c.getTime());
-            ret.setNumeroParcela(i);
-            ret.setDataEnvio(novaDataEnvio.toString());
+                // Adiciona 30 dias para cada data 
+                c.add(Calendar.DAY_OF_MONTH, 30 * i);
+                String novaDataEnvio = sd.format(c.getTime());
+                ret.setNumeroParcela(i);
+                ret.setDataEnvio(novaDataEnvio.toString());
 
-            System.out.println(ret.output());
+                System.out.println(ret.output());
+                aux += ret.toString() + "\r\n";
+                dao.insert(ret);
+            }
 
-                gravarArquivoTxt(ret.toString(), "Arquivo teste da boba serena.txt");
         }
-        }
+        gravarArquivoTxt(aux, "Arquivo teste da boba serena.txt");
 
     }
 
