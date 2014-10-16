ee2eb0169f85f6ae28e5c5190b6f4c78abe7417e
fillipquesado <esquesado@gmail.com>
2014-10-16 09:40:52 -0300

    Correção do autor da classe.

--- src/credicard/ArquivoRetorno.java
+++ src/credicard/ArquivoRetorno.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package credicard;
 
 import fjn.edu.br.Model.Retorno;
@@ -17,7 +12,7 @@ import java.util.List;
 
 /**
  *
- * @author UelioNobre
+ * @author Uelio Nobre
  */
 public class ArquivoRetorno {
 
